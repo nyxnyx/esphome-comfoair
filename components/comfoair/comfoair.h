f8f3ee2c05486b13545ba3350e15a09852977d9b
Grzegorz Szostak <gszostak@neubloc.net>
2026-01-31 01:01:40 +0100

    Update set_name method to include length parameter

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -182,7 +182,7 @@ public:
     this->write_command_(COMFOAIR_SET_RESET_REQUEST, reset_cmd, sizeof(reset_cmd));
 	}
 
-  void set_name(const char* value) {this->name = value;}
+  void set_name(const char* value, int len) {strncpy(this->name, value, len);}
   void set_uart_component(uart::UARTComponent *parent) {this->set_uart_parent(parent);}
 
 protected:
